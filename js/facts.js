5cde1959a0dc8161177cababb0f66657d7271e69
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-11-04 14:03:01 +0100

    Commit progress

--- js/facts.js
+++ js/facts.js
@@ -2,10 +2,25 @@ import { fetchTypes, fetchEncounters, fetchEncounterDetails } from "./api.js";
 
 const typesContainer = document.getElementById('types-container');
 const encountersContainer = document.getElementById('encounters-container');
+const factsContainer = document.getElementById('fact-container');
 const alltypes = await fetchTypes();
+const pokemonFacts = [
+    {
+      text: "Pikachu's original Japanese name is 'Rikachu.' The name was changed to 'Pikachu' when the Pokémon franchise was localized for the international market.",
+      imageSrc: "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/25.svg",
+      alt: "Pikachu Image"
+    },
+    {
+      text: "Magikarp is often considered one of the weakest Pokémon. However, it evolves into Gyarados, a powerful Water/Flying-type Pokémon known for its strength and intimidating appearance.",
+      imageSrc: "https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/129.svg",
+      alt: "Magikarp Image"
+    },
+  ];
 
 displayTypes();
 displayEncounters();
+createRandomFact();
+
 
 function displayTypes() {
     alltypes.forEach(type => {
@@ -36,3 +51,15 @@ async function displayEncounters() {
         container.classList.add('fact-list-style');
     }
 }
+
+function createRandomFact() {
+    const facts = document.createElement('div');
+    const factText = document.createElement('p');
+    const factImage = document.createElement('img');
+    factText.textContent = pokemonFacts[0].text;
+    factImage.src = pokemonFacts[0].imageSrc;
+    facts.appendChild(factText);
+    facts.appendChild(factImage);
+    factsContainer.appendChild(facts);
+}
+
